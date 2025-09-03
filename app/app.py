b6d043c340f77d209d7d097f9876c9d263e3a003
Vikas Tiwari <114413363+vikas809028@users.noreply.github.com>
2025-09-04 00:51:59 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins Docker Demo!"
+    return "Hello from Jenkins Docker Demo for trigger webhook!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
